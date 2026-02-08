6900bc66bfc7e87fc078afff2541b6c92ddc6b9e
catherine <catherinepgeorge02@gmail.com>
2026-02-08 21:01:30 +0530

    Modified file names of employee

--- login.js
+++
@@ -1,9 +0,0 @@
-function loginBtn() {
-    username = document.getElementById('user').value;
-
-    // store the value in database
-    localStorage.setItem('USERNAME',username);
-
-    //redirect to index page
-    window.location = './index.html'
-}
